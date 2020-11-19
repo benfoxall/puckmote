fc54c9ed566f64d120c982d53aeb086c035ec077
Ben Foxall <benfoxall@gmail.com>
2020-11-19 23:47:04 +0000

    ./

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,6 @@ render(<App />, document.querySelector("#root"));
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("../sw.js");
+    navigator.serviceWorker.register("sw.js");
   });
 }
