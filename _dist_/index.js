3a00f68dc929ad7419a2609a86d9f6ff8d85e8ba
benfoxall <benfoxall@gmail.com>
2020-11-20 00:10:10 +0000

    Deploying to gh-pages from  @ 304913b37299addca94b1cbab945f41f865e90e3 🚀

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,8 +2,3 @@ import React from "../web_modules/react.js";
 import {render} from "../web_modules/react-dom.js";
 import {App as App2} from "./App.js";
 render(/* @__PURE__ */ React.createElement(App2, null), document.querySelector("#root"));
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js");
-  });
-}
