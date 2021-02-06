25890a3d4413396ef429f177ab10a97d14ef0d5e
Ben Foxall <benfoxall@gmail.com>
2021-02-06 21:20:10 +0000

    more styling

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,11 @@ export const App = () => {
 
   return (
     <>
-      <h1 className="text-4xl p-4">Puckmote</h1>
-      <h1 className="text-2xl p-4">Samsung</h1>
-      <div className="flex flex-wrap p-4">
+      <h1 className="text-4xl m-4 text-gray-900 dark:text-white">Puckmote</h1>
+      <h1 className="text-2xl m-4">Samsung</h1>
+      <div className="flex flex-wrap m-2 max-w-4xl">
         {Object.entries(SamsungTV).map(([name, prontoHex]) => (
-          <button key={name} onClick={() => run(prontoHex)} className="flex whitespace-nowrap items-center justify-center rounded-full bg-purple-700 hover:bg-red-700 text-white p-3 m-2 h-10 hover:bg-red-700">
+          <button key={name} onClick={() => run(prontoHex)} className="flex whitespace-nowrap items-center justify-center rounded-full bg-blue-500 hover:bg-red-700 text-white p-3 m-2 h-10 hover:bg-blue-700 focus:bg-blue-700 transition-all ">
             {name}
           </button>
         ))}
