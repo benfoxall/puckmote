5cb1bfa6514e1e6dce604c79804efcf5b5574c5c
Ben Foxall <benfoxall@gmail.com>
2021-05-02 17:01:26 +0000

    Load button list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, { ChangeEventHandler, useState } from "react";
-import { PuckProvider, PuckStatus, usePuckRepl } from './react-puck'
+import React, {
+  ChangeEventHandler,
+  FC,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { PuckProvider, PuckStatus, usePuckRepl } from "./react-puck";
 
 import { SamsungTV } from "./data";
-import { useIRDBData } from "./irdb";
+import { CSVRow, fetchIRDBDevice, useIRDBData } from "./irdb";
 import { pronto } from "./lib/pronto";
-
 
 const Wrapped = () => {
   const repl = usePuckRepl();
@@ -32,57 +37,119 @@ const Wrapped = () => {
       <h1 className="text-2xl m-4">Samsung</h1>
       <div className="flex flex-wrap m-2 max-w-4xl">
         {Object.entries(SamsungTV).map(([name, prontoHex]) => (
-          <button key={name} onClick={() => run(prontoHex)} className="flex whitespace-nowrap items-center justify-center rounded bg-green-300 p-4 m-1 h-6 hover:bg-green-700 focus:bg-green-700 transition-all text-l font-bold">
+          <button
+            key={name}
+            onClick={() => run(prontoHex)}
+            className="flex whitespace-nowrap items-center justify-center rounded bg-green-300 p-4 m-1 h-6 hover:bg-green-700 focus:bg-green-700 transition-all text-l font-bold"
+          >
             {name}
           </button>
         ))}
       </div>
     </div>
   );
-}
+};
 
-
-export const AppV1 = () => <PuckProvider>
-  <Wrapped />
-</PuckProvider>
-
+export const AppV1 = () => (
+  <PuckProvider>
+    <Wrapped />
+  </PuckProvider>
+);
 
 export const AppV2 = () => {
+  const manufacturers = useIRDBData();
+  const [manufacturer, setManufacturer] = useState<string>();
+  const changeManufacturer: ChangeEventHandler<HTMLSelectElement> = (e) =>
+    setManufacturer(e.target.value);
 
-  const manufacturers = useIRDBData()
-  const [manufacturer, setManufacturer] = useState<string>()
-  const changeManufacturer: ChangeEventHandler<HTMLSelectElement> = (e) => setManufacturer(e.target.value)
+  const types = manufacturers[manufacturer];
+  const [type, setType] = useState<string>();
+  const changeType: ChangeEventHandler<HTMLSelectElement> = (e) =>
+    setType(e.target.value);
 
-  const types = manufacturers[manufacturer]
-  const [type, setType] = useState<string>()
-  const changeType: ChangeEventHandler<HTMLSelectElement> = (e) => setType(e.target.value)
+  const devices = types?.[type];
 
-  const items = types?.[type]
+  const submit = (e) => e.preventDefault();
 
-
-  return <form className="m-5">
-    <select className="bg-gray-800 m-2 p-2 rounded" onChange={changeManufacturer} value={manufacturer}>
-      <option>…</option>
-      {Object.keys(manufacturers).map
-        (name => <option key={name}>{name}</option>)}
-    </select>
-
-
-    {types &&
-      <select className="bg-gray-800 m-2 p-2 rounded block" onChange={changeType} value={type}>
-        {Object.keys(types).map
-          (name => <option key={name}>{name}</option>)}
+  return (
+    <form className="m-5 font-mono" onSubmit={submit}>
+      <select
+        className="bg-gray-800 m-2 p-2 rounded"
+        onChange={changeManufacturer}
+        name="manufacturer"
+        value={manufacturer}
+      >
+        <option>…</option>
+        {Object.keys(manufacturers).map((name) => (
+          <option key={name}>{name}</option>
+        ))}
       </select>
-    }
 
-    <ul className="bg-gray-800 m-2 my-8 p-2 rounded">
-      {items?.map(([dev, subdev], i) => <li key="i">{dev}, {subdev}</li>)}
-    </ul>
+      {types && (
+        <select
+          className="bg-gray-800 m-2 p-2 rounded block"
+          onChange={changeType}
+          name="deviceType"
+          value={type}
+        >
+          <option>…</option>
+          {Object.keys(types).map((name) => (
+            <option key={name}>{name}</option>
+          ))}
+        </select>
+      )}
 
-  </form>
+      <ul>
+        {devices?.map(([dev, subdev], i) => (
+          <li key={i}>
+            <div className="mx-2 mt-8 p-2 rounded text-right opacity-20">
+              {dev} | {subdev}
+            </div>
+            <div className="bg-gray-800 p-2 rounded">
+              <Device
+                manufacturer={manufacturer}
+                devicetype={type}
+                device={dev}
+                subdevice={subdev}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
 
-}
+const Device: FC<{
+  manufacturer: string;
+  devicetype: string;
+  device: string;
+  subdevice: string;
+}> = ({ manufacturer, devicetype, device, subdevice }) => {
+  const [butts, setButts] = useState<CSVRow[]>();
 
+  useEffect(() => {
+    setButts([]);
+    fetchIRDBDevice(manufacturer, devicetype, device, subdevice).then(setButts);
+  }, [manufacturer, devicetype, device, subdevice]);
 
+  if (butts) {
+    return (
+      <nav className="flex flex-wrap">
+        {butts.map((row, i) => (
+          <button
+            className="m-2 p-2 bg-gray-900 rounded shadow hover:text-pink-500 focus:text-pink-500"
+            key={i}
+            type="button"
+          >
+            {row.functionname}
+          </button>
+        ))}
+      </nav>
+    );
+  } else {
+    return <p>data</p>;
+  }
+};
 
-export const App = AppV1
+export const App = AppV2;
--- src/irdb.ts
+++ src/irdb.ts
@@ -1,53 +1,99 @@
 import { useEffect, useState } from "react";
+import Papa from "papaparse";
 
-const ENDPOINT = 'https://cdn.jsdelivr.net/gh/probonopd/irdb@master/codes/index';
+const ENDPOINT = "https://cdn.jsdelivr.net/gh/probonopd/irdb@master/codes/";
 
-const re = /^(?<manufacturer>.+)\/(?<devicetype>.+)\/(?<device>.+),(?<subdevice>.+)\.csv$/
+const re = /^(?<manufacturer>.+)\/(?<devicetype>.+)\/(?<device>.+),(?<subdevice>.+)\.csv$/;
 
-interface Device {
-  manufacturer: string;
-  devicetype: string;
-  device: string;
-  subdevice: string;
+interface REGroups extends RegExpMatchArray {
+  groups: {
+    manufacturer: string;
+    devicetype: string;
+    device: string;
+    subdevice: string;
+  };
 }
 
 type IIRDBDATA = {
   [manufacturer: string]: {
-    [devicetype: string]: [device: string, subdevice: string][]
-  }
-}
+    [devicetype: string]: [device: string, subdevice: string][];
+  };
+};
 
+const fetchIRDBIndex = async () => {
+  const res = await fetch(ENDPOINT + "index");
+  const text = await res.text();
 
-const fetchIRDBData = async () => {
-
-  const res = await fetch(ENDPOINT)
-  const text = await res.text()
-
-
-  const data: IIRDBDATA = {}
+  const data: IIRDBDATA = {};
 
   for (const line of text.split("\n")) {
-    const match = line.match(re)
+    const match = line.match(re) as REGroups;
     if (match) {
-      const device = match.groups as any as Device
+      const device = match.groups;
 
-      const man = data[device.manufacturer] ||= {}
-      const dev = man[device.devicetype] ||= []
-      dev.push([device.device, device.subdevice])
-
+      const man = (data[device.manufacturer] ||= {});
+      const dev = (man[device.devicetype] ||= []);
+      dev.push([device.device, device.subdevice]);
     }
   }
 
   return data;
-
-}
+};
 
 export const useIRDBData = () => {
-  const [data, setData] = useState<IIRDBDATA>({})
+  const [data, setData] = useState<IIRDBDATA>({});
 
   useEffect(() => {
-    fetchIRDBData().then(setData)
-  }, [])
+    fetchIRDBIndex().then(setData);
+  }, []);
 
-  return data
+  return data;
+};
+
+// INPUT SOURCE,NECx2,7,7,1
+export interface CSVRow {
+  device: string;
+  function: string;
+  functionname: string;
+  protocol: string;
+  subdevice: string;
 }
+
+export const fetchIRDBDevice = async (
+  manufacturer: string,
+  devicetype: string,
+  device: string,
+  subdevice: string
+): Promise<CSVRow[]> => {
+  const path = `${ENDPOINT}${manufacturer}/${devicetype}/${device},${subdevice}.csv`;
+
+  return new Promise((resolve) => {
+    Papa.parse<CSVRow>(path, {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete(result) {
+        resolve(result.data);
+      },
+    });
+  });
+
+  // const res = await fetch(ENDPOINT + path)
+  // const text = await res.text()
+
+  // const data: IIRDBDATA = {}
+
+  // for (const line of text.split("\n")) {
+  //   const match = line.match(re) as REGroups
+  //   if (match) {
+  //     const device = match.groups;
+
+  //     const man = data[device.manufacturer] ||= {}
+  //     const dev = man[device.devicetype] ||= []
+  //     dev.push([device.device, device.subdevice])
+
+  //   }
+  // }
+
+  // return data;
+};
