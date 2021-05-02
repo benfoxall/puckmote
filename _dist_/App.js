90053effead516b54c66b2b6ddbce0adbfb8b406
benfoxall <benfoxall@gmail.com>
2021-05-02 09:46:33 +0000

    Deploying to gh-pages from  @ 1f961133c7465d6e7540ff1e627fedd26d6527c1 🚀

--- _dist_/App.js
+++ _dist_/App.js
@@ -1,6 +1,7 @@
-import React from "../web_modules/react.js";
+import React, {useState} from "../web_modules/react.js";
 import {PuckProvider, PuckStatus, usePuckRepl} from "./react-puck.js";
 import {SamsungTV} from "./data.js";
+import {useIRDBData} from "./irdb.js";
 import {pronto as pronto2} from "./lib/pronto.js";
 const Wrapped = () => {
   const repl = usePuckRepl();
@@ -16,7 +17,7 @@ const Wrapped = () => {
     `);
   };
   return /* @__PURE__ */ React.createElement("div", {
-    className: "container mx-auto max-w-md p-8 m-8 bg-gray-200 shadow-2xl rounded"
+    className: "container mx-auto max-w-md p-8 m-8 bg-gray-200 text-black shadow-2xl rounded"
   }, /* @__PURE__ */ React.createElement("div", {
     className: "m-4"
   }, /* @__PURE__ */ React.createElement(PuckStatus, null)), /* @__PURE__ */ React.createElement("h1", {
@@ -28,7 +29,36 @@ const Wrapped = () => {
   }, Object.entries(SamsungTV).map(([name, prontoHex]) => /* @__PURE__ */ React.createElement("button", {
     key: name,
     onClick: () => run(prontoHex),
-    className: "flex whitespace-nowrap items-center justify-center rounded-full bg-green-300 p-4 m-1 h-6 hover:bg-green-700 focus:bg-green-700 transition-all text-l font-bold"
+    className: "flex whitespace-nowrap items-center justify-center rounded bg-green-300 p-4 m-1 h-6 hover:bg-green-700 focus:bg-green-700 transition-all text-l font-bold"
   }, name))));
 };
-export const App = () => /* @__PURE__ */ React.createElement(PuckProvider, null, /* @__PURE__ */ React.createElement(Wrapped, null));
+export const AppV1 = () => /* @__PURE__ */ React.createElement(PuckProvider, null, /* @__PURE__ */ React.createElement(Wrapped, null));
+export const AppV2 = () => {
+  const manufacturers = useIRDBData();
+  const [manufacturer, setManufacturer] = useState();
+  const changeManufacturer = (e) => setManufacturer(e.target.value);
+  const types = manufacturers[manufacturer];
+  const [type, setType] = useState();
+  const changeType = (e) => setType(e.target.value);
+  const items = types?.[type];
+  return /* @__PURE__ */ React.createElement("form", {
+    className: "m-5"
+  }, /* @__PURE__ */ React.createElement("select", {
+    className: "bg-gray-800 m-2 p-2 rounded",
+    onChange: changeManufacturer,
+    value: manufacturer
+  }, /* @__PURE__ */ React.createElement("option", null, "\u2026"), Object.keys(manufacturers).map((name) => /* @__PURE__ */ React.createElement("option", {
+    key: name
+  }, name))), types && /* @__PURE__ */ React.createElement("select", {
+    className: "bg-gray-800 m-2 p-2 rounded block",
+    onChange: changeType,
+    value: type
+  }, Object.keys(types).map((name) => /* @__PURE__ */ React.createElement("option", {
+    key: name
+  }, name))), /* @__PURE__ */ React.createElement("ul", {
+    className: "bg-gray-800 m-2 my-8 p-2 rounded"
+  }, items?.map(([dev, subdev], i) => /* @__PURE__ */ React.createElement("li", {
+    key: "i"
+  }, dev, ", ", subdev))));
+};
+export const App = AppV1;
